3c61dc48dbb9b26499b9826f30078c8ebeafbaf3
Hiroki Chiyoda <contact@arafipro.com>
2025-05-26 09:07:15 +0900

    サインインボタンの背景色を青から緑に変更

--- app/(main)/_components/signin-button.tsx
+++ app/(main)/_components/signin-button.tsx
@@ -15,7 +15,7 @@ export default function SigninButton({
       variant={"outline"}
       asChild
       className={cn(
-        "text-xl text-white px-10 py-6 shadow-lg hover:shadow-xl bg-gradient-to-r from-blue-600 to-blue-800 transform hover:scale-105 transition-all duration-200",
+        "text-xl text-white px-10 py-6 shadow-lg hover:shadow-xl bg-gradient-to-r from-green-600 to-green-800 transform hover:scale-105 transition-all duration-200",
         className
       )}
     >
