fc92d93f14ae2d9a336b425e5b2767b0c3960a25
Hiroki Chiyoda <contact@arafipro.com>
2025-05-26 16:25:48 +0900

    チェックアウトセッションの作成機能を追加

---
+++ app/api/checkout-sessions/route.ts
@@ -0,0 +1,20 @@
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { stripe } from "../../../lib/stripe";
+
+export async function POST() {
+  const headersList = await headers();
+  const origin = headersList.get("origin");
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: "price_1RSsv9AmDSQJ0iJl5KrqkaZi",
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${origin}/?canceled=true`,
+  });
+  return NextResponse.redirect(session.url!, 303);
+}
